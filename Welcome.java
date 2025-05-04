c8ea485e27535a3a71b1e18a992367ca5b05c883
AnaCarinaPP <ancari1989@gmail.com>
2025-05-04 16:03:06 +0200

    add feature welcome

--- Welcome.java
+++ Welcome.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
 //Inicializa la clase Welcome
-//Crea el método main
-
+public class Welcome {
+    //Crea el método main
+    public static void main(String[] args) {
+        //greeting();
+        greetingCoder();
+    }
 //Tarea 1
 //Define el método greeting que imprima en terminal ¡Hola FemCoder!
-//Ejecuta el método greeting 
-
+//Ejecuta el método greeting
+    public static void greeting () {
+        System.out.print("¡Hola FemCoder!");
+    }
 //Tarea 2
 //Define el método greetingCoder que reciba un nombre y que imprima en terminal ¡Hola <name>!
-//Ejecuta el método greetingCoder 
-
+    public static void greetingCoder (){
+        Scanner greeting = new Scanner(System.in);
+        System.out.println("¿Cómo te llamas?");
+        String name = greeting.nextLine();
+        greeting.close();
+        System.out.println("¡Hola "+name+"!");
+    }
+}
+//Ejecuta el método greetingCoder
 //¿Sabes donde definir el método y en dónde lo has de ejecutar? Si no lo sabes vuelve al archivo de Resources.md
